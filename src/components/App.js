35f0d4e76d6a6edec0fe00a9f0c56ed978d4d57a
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-10-09 19:31:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,33 @@ import React from "react";
 import "../styles/App.css";
 import { useState, useEffect } from "react";
 const App = () => {
-  //code here
+   //code here
   const [name, setName] = useState([]);
+  const [id, setId] = useState(1);
 
-  // const callApi = async () => {
-  //   const response = await fetch(
-  //     "https://content.newtonschool.co/v1/pr/main/users"
-  //   );
-  //   const finalData = await JSON(response);
-  //   console.log(finalData);
-  // };
-
-  const changeInput = () => {
-    //setName(finalData.name);
+  const callApi = async () => {
+    const response = await fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`);
+    const finalData = await response.json();
+    console.log(finalData);
+    setName(finalData);
   };
 
-  // useEffect(() => {
-  //   callApi();
-  // }, []);
+  const changeInput = (event) => {
+    setId(event.target.value);
+  };
 
+  useEffect(() => {
+    callApi();
+  },[id]);
+  
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
       <input id="input" onChange={changeInput} />
-      <p id="name">{name}</p>
+      <p id="name">{name.name}</p>
     </div>
   );
 };
+
 
 export default App;
